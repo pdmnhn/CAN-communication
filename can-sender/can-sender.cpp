ae9ab1b066f80e21fab870318338dbbb6cf55178
Padmanabhan <42073602+pdmnhn@users.noreply.github.com>
2023-03-09 22:10:04 +0530

    First review

--- can-sender/can-sender.cpp
+++ can-sender/can-sender.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include <string>
+#include <cstring>
+#include <stdint.h>
 #include "pico/stdlib.h"
 #include "pico/time.h"
 #include "hardware/adc.h"
@@ -12,43 +15,76 @@ const uint ADC_RANGE = 1 << 12;
 const uint LED = 25;
 const double ADC_VREF = 3.3;
 const double ADC_CONVERSION_FACTOR = ADC_VREF / (ADC_RANGE - 1);
+const unsigned char MAC[8] = {0xFF, 0xDD, 0xCC, 0xBB, 0x00, 0x11, 0x22, 0x33};
+
+uint32_t getCanID(uint16_t dataId, uint16_t counter, uint8_t command)
+{
+    return dataId << 21 | (0b11 << 19) | (command << 17) | (counter << 1) | CAN_EFF_FLAG;
+}
 
 int main()
 {
     stdio_init_all();
+    const uint16_t temperatureId = 0b01001100110;
+    uint16_t counter = 0;
 
     MCP2515 canInterface;
     can_frame frame;
-    frame.can_id = 0x000;
+    frame.can_id;
 
-    // Initialize interface
     canInterface.reset();
     canInterface.setBitrate(CAN_1000KBPS, MCP_16MHZ);
     canInterface.setNormalMode();
+
     adc_init();
     adc_gpio_init(ADC_PIN);
     adc_select_input(ADC_CHN);
     gpio_init(LED);
     gpio_set_dir(LED, GPIO_OUT);
 
-    double reading, temperature;
-    string temperatureString;
+    double reading;
+    unsigned char temperature = 0;
+    // string temperatureString;
 
     while (true)
     {
-        gpio_put(LED, 1);
         reading = adc_read() * ADC_CONVERSION_FACTOR;
         temperature = 27 - (reading - 0.706) / 0.001721;
-        temperatureString = to_string(temperature);
-        for (uint i = 0; i < temperatureString.length(); i++)
+        // temperature = (temperature + 1) % 10000;
+        // temperatureString = to_string(temperature);
+
+        // for (int i = 0; i < temperatureString.length(); i++)
+        // {
+        //     frame.data[i] = temperatureString[i];
+        //     cout << frame.data[i] << ", ";
+        // }
+        // cout << endl;
+
+        frame.data[0] = temperature;
+        cout << "Temperature: " << (int)temperature << endl;
+        frame.can_dlc = 1;
+
+        frame.can_id = getCanID(temperatureId, counter, 0b00);
+
+        if (canInterface.sendMessage(&frame) == MCP2515::ERROR_OK)
         {
-            frame.data[i] = temperatureString[i];
+            frame.can_id = getCanID(temperatureId, counter, 0b01);
+            frame.can_dlc = 8;
+            for (int i = 0; i < 8; i++)
+            {
+                frame.data[i] = MAC[i];
+            }
+            if (canInterface.sendMessage(&frame) == MCP2515::ERROR_OK)
+            {
+                cout << "Success: " << counter << endl
+                     << endl;
+                gpio_put(LED, 1);
+                sleep_ms(2500);
+                gpio_put(LED, 0);
+                sleep_ms(2500);
+            }
         }
-        frame.can_dlc = temperatureString.length();
-        canInterface.sendMessage(&frame);
-        sleep_ms(5000);
-        gpio_put(LED, 0);
-        sleep_ms(5000);
+        counter = (counter + 1) % 0xFFFF;
     }
 
     return 0;
